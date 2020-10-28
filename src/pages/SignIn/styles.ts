f587e65cbd8425e578286078e451394c7d1a24ec
FranciscoMaik <chiconunes3081@gmail.com>
2020-10-28 18:04:53 -0300

    Isolating components

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -21,45 +21,11 @@ export const Content = styled.div`
 
   form {
     margin: 80px 0;
-    width: 240px;
+    width: 280px;
     text-align: center;
 
     h1 {
       margin-bottom: 24px;
-    }
-
-    input {
-      background: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      width: 100%;
-      color: #f4ede8;
-
-      &::placeholder {
-        color: #666360;
-      }
-
-      & + input {
-        margin-top: 8px;
-      }
-    }
-
-    button {
-      background: #ff9000;
-      height: 56px;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      color: #312e38;
-      width: 100%;
-      font-weight: 500;
-      margin-top: 16px;
-      transition: background-color 0.2s;
-
-      &:hover {
-        background: ${shade(0.2, '#ff9000')};
-      }
     }
 
     a {
