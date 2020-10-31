f52aa1be6060417427c7c4652d743d69c0d186b2
FranciscoMaik <chiconunes3081@gmail.com>
2020-10-30 21:47:39 -0300

    Alert error with Tooltip

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -22,6 +25,12 @@ export const Container = styled.div<ContainerProps>`
   }
 
   ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
     props.isFocused &&
     css`
       color: #ff9000;
@@ -35,7 +44,7 @@ export const Container = styled.div<ContainerProps>`
     `}
 
   input {
-    flex: 1;
+    width: 90%;
     background: transparent;
     border: 0;
     color: #f4ede8;
@@ -47,5 +56,22 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 10px;
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
