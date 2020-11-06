66b2d20b2edb576f88073364e0f21c44bcaf703a
FranciscoMaik <chiconunes3081@gmail.com>
2020-11-06 16:07:40 -0300

    Validation routes and redirect

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
-// import { Container } from './styles';
+import Route from './Routes';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 };
